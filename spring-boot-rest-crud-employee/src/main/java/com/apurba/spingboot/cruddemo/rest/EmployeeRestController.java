b906e93837b45e9d52fca250aa811148205d0f5e
Apurba Debnath <apurba871@gmail.com>
2026-02-01 08:26:44 +0530

    add functionality to save a new employee

--- spring-boot-rest-crud-employee/src/main/java/com/apurba/spingboot/cruddemo/rest/EmployeeRestController.java
+++ spring-boot-rest-crud-employee/src/main/java/com/apurba/spingboot/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.apurba.spingboot.cruddemo.dao.EmployeeDAO;
 import com.apurba.spingboot.cruddemo.entity.Employee;
 import com.apurba.spingboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,21 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    //expose "/employees/{employeeId}" and return a single employee
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null)
+            throw new RuntimeException("Employee id not found " + employeeId);
+        return theEmployee;
+    }
+
+    //expose "/employees" to add a new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employeeDetails) {
+        Employee newEmployee = employeeService.save(employeeDetails);
+        return newEmployee;
     }
 }
